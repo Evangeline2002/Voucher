4baebf1538906efdb7fd66948842b00b1f30cb24
Evangeline2002 <evangelinejeba62@gmail.com>
2026-08-08 16:36:05 +0530

    first commit

--- src/components/PaymentVoucherForm.jsx
+++ src/components/PaymentVoucherForm.jsx
@@ -9,10 +9,22 @@ const PaymentVoucherForm = ({ data, onChange, onSave, onClear, onPrint, onDownlo
             <h2 className="form-header">Payment Voucher Form</h2>
 
             <div className="form-actions">
-                <button type="button" className="btn btn-save" onClick={onSave}>Save</button>
-                <button type="button" className="btn btn-clear" onClick={onClear}>Clear</button>
-                <button type="button" className="btn btn-print" onClick={onPrint}>Print Voucher</button>
-                <button type="button" className="btn btn-pdf" onClick={onDownloadPDF}>Download PDF</button>
+                <button type="button" className="btn btn-save" onClick={onSave}>
+                    <svg width="18" height="18" fill="currentColor" viewBox="0 0 16 16"><path d="M2 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4.207a1 1 0 0 0-.293-.707l-2.5-2.5A1 1 0 0 0 12.5 1H2zm0 1h10.086l2 2V13a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1z" /><path d="M4 1.5A.5.5 0 0 1 4.5 1h7a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5v-3zM4.5 2v2h6V2h-6zM3.5 9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5V9zm1 .5v3h6v-3h-6z" /></svg>
+                    Save
+                </button>
+                <button type="button" className="btn btn-clear" onClick={onClear}>
+                    <svg width="18" height="18" fill="currentColor" viewBox="0 0 16 16"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" /><path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" /></svg>
+                    Clear
+                </button>
+                <button type="button" className="btn btn-print" onClick={onPrint}>
+                    <svg width="18" height="18" fill="currentColor" viewBox="0 0 16 16"><path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z" /><path d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2H5zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4V3zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2H5zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1z" /></svg>
+                    Print Voucher
+                </button>
+                <button type="button" className="btn btn-pdf" onClick={onDownloadPDF}>
+                    <svg width="18" height="18" fill="currentColor" viewBox="0 0 16 16"><path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" /><path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" /></svg>
+                    Download PDF
+                </button>
             </div>
 
             <form onSubmit={e => e.preventDefault()} className="voucher-form">
