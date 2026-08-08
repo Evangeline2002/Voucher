e3d3cb25c643d7be4df49a96a78ebf76c0e1a43b
Evangeline2002 <evangelinejeba62@gmail.com>
2026-08-08 16:33:00 +0530

    first commit

--- src/components/PaymentVoucherForm.jsx
+++ src/components/PaymentVoucherForm.jsx
@@ -7,18 +7,18 @@ const PaymentVoucherForm = ({ data, onChange, onSave, onClear, onPrint, onDownlo
     return (
         <div className="voucher-form-container hide-on-print">
             <h2 className="form-header">Payment Voucher Form</h2>
-            <form onSubmit={e => e.preventDefault()} className="voucher-form">
 
+            <div className="form-actions">
+                <button type="button" className="btn btn-save" onClick={onSave}>Save</button>
+                <button type="button" className="btn btn-clear" onClick={onClear}>Clear</button>
+                <button type="button" className="btn btn-print" onClick={onPrint}>Print Voucher</button>
+                <button type="button" className="btn btn-pdf" onClick={onDownloadPDF}>Download PDF</button>
+            </div>
+
+            <form onSubmit={e => e.preventDefault()} className="voucher-form">
                 <CompanyDetails data={data} onChange={onChange} />
                 <PaymentDetails data={data} onChange={onChange} />
                 <SignatureSection data={data} onChange={onChange} />
-
-                <div className="form-actions">
-                    <button type="button" className="btn btn-save" onClick={onSave}>Save</button>
-                    <button type="button" className="btn btn-clear" onClick={onClear}>Clear</button>
-                    <button type="button" className="btn btn-print" onClick={onPrint}>Print Voucher</button>
-                    <button type="button" className="btn btn-pdf" onClick={onDownloadPDF}>Download PDF</button>
-                </div>
             </form>
         </div>
     );
